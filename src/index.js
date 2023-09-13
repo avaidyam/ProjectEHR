94ef4ac1053c830b54864f213359b7325233e717
Anthony Bosshardt <anthony.bosshardt@herophilus.com>
2023-09-13 17:39:26 -0500

    rescope existing css; expand dir structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { StyledEngineProvider } from "@mui/material/styles";
-import Notewriter from "./Notewriter";
-import "./style.css";
+
+import Notewriter from "./components/notes/Notewriter";
+
+import "./index.css";
 
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <Notewriter />
     </StyledEngineProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
