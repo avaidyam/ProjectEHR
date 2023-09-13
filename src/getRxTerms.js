202ea5b7d69207a3065b4f50aa48e37a8a6e0485
aw449 <awong7905@gmail.com>
2023-09-13 16:31:04 -0500

    Refactor formatRxTerms to use map instead of forEach
    refactored the formatRxTerms function to use the map() method instead of the forEach() loop.

--- src/getRxTerms.js
+++ src/getRxTerms.js
@@ -14,21 +14,14 @@ async function getRxTerms(searchTerm) {
 }
 
 function formatRxTerms(data) {
-   
-    // Call the getMedicalCodes function to get the data
-    // Initialize an empty array to store the formatted results
-    const formattedResult = [];
-
-    // Iterate through each drug and its strengths
-    data[1].forEach((drug, index) => {
-        const id = data[2]["RXCUIS"][index]
+    const formattedResult = data[1].map((drug, index) => {
+        const id = data[2]["RXCUIS"][index];
         const name = drug;
         const dose = data[2]["STRENGTHS_AND_FORMS"][index];
         const route = getRouteFromDose(dose);
         const frequency = "string"; // Set frequency as needed
         const refills = 0; // Set refills as needed
 
-            // Create the formatted entry and push it to the array
         const formattedEntry = {
             id,
             name,
@@ -40,11 +33,12 @@ function formatRxTerms(data) {
             },
         };
 
-        formattedResult.push(formattedEntry);
+        return formattedEntry;
     });
 
     return formattedResult;
-} 
+}
+
 
 // Function to extract the route from the dose
 function getRouteFromDose(dose) {
