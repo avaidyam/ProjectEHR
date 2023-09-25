5274cac0ef5df6e70f5f5e6f9d2c37a17391e4b8
aw449 <awong7905@gmail.com>
2023-09-25 16:06:36 -0500

    Moved .txt into comment
    Moved .txt into comment.

--- src/util/getRxTerms.js
+++ src/util/getRxTerms.js
@@ -1,3 +1,52 @@
+/*SAMPLE OUTPUT for search term: Aspirin
+
+Format is as follows 
+id: This is the SX_RXCUI which is unique for each Name+Route combination of a drug on the RxTerms database.
+name: Name of drug
+fields.dose: The dose of the drug is returned in the same order as the corresponding route list
+fields.route: The route of the drug
+fields.frequency: placeholder
+fields.refills: placeholder
+
+[
+  {
+    "id": [
+      "1154070",
+      "1294937",
+      "1154071"
+    ],
+    "name": "Aspirin",
+    "fields": {
+      "dose": [
+        [
+          " 81 mg Cap",
+          " 81 mg DR Tab",
+          " 81 mg Tab",
+          "325 mg Cap",
+          "325 mg DR Tab",
+          "325 mg Tab",
+          "500 mg DR Tab",
+          "500 mg Tab",
+          "650 mg DR Tab",
+          "650 mg Tab"
+        ],
+        [
+          "81 mg Tab"
+        ],
+        [
+          "300 mg Suppository",
+          "600 mg Suppository"
+        ]
+      ],
+      "route": [
+        "Oral Pill",
+        "Chewable",
+        "Rectal"
+      ],
+      "frequency": "string",
+      "refills": 0
+    }
+  },*/
 export async function getRxTerms(searchTerm) {
     try {
         const rxtermsApiUrl = `https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?terms=${searchTerm}&ef=STRENGTHS_AND_FORMS,RXCUIS`;
