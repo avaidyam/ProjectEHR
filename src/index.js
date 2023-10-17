ff0a0c727a700f9e64cbb86bd481277d40c0a8ea
kkyl <kirklbery@gmail.com>
2023-10-17 00:16:28 +0000

    merging main

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>CIMED EHR Project</title>
-    <meta name="viewport" content="initial-scale=1, width=device-width" />
-    <!-- Fonts to support Material Design -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
-    />
-    <!-- Icons to support Material Design -->
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    />
-  </head>
-  <body>
-    <div id="root"></div>
-  </body>
-</html>
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import RootRouter from './RootRouter.js';
+
+import './index.css';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const rootContainer = document.getElementById('root');
+const rootNode = ReactDOM.createRoot(rootContainer);
+
+rootNode.render(
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <RootRouter />
+    </ThemeProvider>
+  </React.StrictMode>
+);
