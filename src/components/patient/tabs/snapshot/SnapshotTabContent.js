dfd609cf841cd20879ae74436881450cb229a950
VarunGop <167945392+VarunGop@users.noreply.github.com>
2024-09-30 17:42:27 +0000

    Changed UI for Patient Snapshot (Issue 15) to closer match EPIC

--- src/components/patient/tabs/snapshot/SnapshotTabContent.js
+++ src/components/patient/tabs/snapshot/SnapshotTabContent.js
@@ -1,6 +1,8 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
 import { usePatientMRN } from '../../../../util/urlHelpers.js';
+import { Icon } from '@mui/material'; // Import your icon from Material UI or any icon library you're using
+
 
 const TEST_PATIENT_INFO = ({ patientMRN }) => ({
   firstName: 'Patient',
@@ -15,11 +17,13 @@ const TEST_PATIENT_INFO = ({ patientMRN }) => ({
   allergies: [{id: 'uiduid', allergen: 'Peanuts', reaction: 'Death'}],
   medicalHistory: [{
       id: 'jakjak',
-      name: 'Opioid Use Disorder'
+      name: 'Opioid Use Disorder',
+      date: 'Date Unknown'
     },
     {
       id: 'whywhy', 
-      name: 'Heart Failure with preserved ejection fraction'
+      name: 'Heart Failure with preserved ejection fraction',
+      date: '9/2/2018'
     }
   ],
   medications: [{
@@ -44,7 +48,7 @@ const TEST_PATIENT_INFO = ({ patientMRN }) => ({
   surgicalHistory: [{
       id: '123123',
       name: 'ulnar collateral reconstruction (R)',
-      date: '2014-01-07' 
+      date: '01/04/2017' 
     },
   ],
   familyHistory: [
@@ -60,16 +64,25 @@ const TEST_PATIENT_INFO = ({ patientMRN }) => ({
 const SnapshotTabContent = ({ children, ...other }) => {
   const [patientMRN, setPatientMRN] = usePatientMRN();
 
-  const TitledCard = ({ children, title}) => {
+  const TitledCard = ({ children, title, color}) => {
     // At some point I will need to use a URLBuilder to link the title to corresponding pages
     // Leaving it for now because I don't know if that's within scope
     return(
       <div style={{ 
-        margin: "1em 1em 1em 1em", 
-        padding: "0.5em 1em 0.5em 1em", 
-        border: "1px solid rgba(255,255,255,0.25)" 
+        borderLeftWidth: 15,
+        borderLeftColor: color,
+        borderLeftStyle: 'solid',  // Define the left border style
+        borderTop: '1px solid #ccc',  // Other borders individually defined
+        borderRight: '1px solid #ccc',
+        borderBottom: '1px solid #ccc', 
+        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)', 
+        marginBottom: '8px',
+        padding: '16px',
+        borderRadius: '8px',
+        overflow: 'hidden',
+        position: 'relative'
       }}>
-        <h3 style={{ fontSize: "1.5em", margin: 0 }}>{title}</h3>
+        <h3 style={{ fontSize: "1.2em", marginTop: 0, color: color}}>{title}</h3>
         <div style={{ margin: 0 }}>{children}</div>
       </div>
     )
@@ -79,46 +92,64 @@ const SnapshotTabContent = ({ children, ...other }) => {
 
   return (
     <div>
-      <TitledCard title="Patient Info">
+      <TitledCard title="Patient" color='#5EA1F8'>
         <b>Name:</b> {ptInfo.firstName + ' ' + ptInfo.lastName}<br/>
         <b>Age:</b> {ptInfo.age}<br/>
         <b>Date of Birth:</b> {ptInfo.dateOfBirth}<br/>
         <b>Address:</b> {ptInfo.address}<br/>
       </TitledCard>
-      <TitledCard title="Allergies">
-        <ul>
-          {ptInfo.allergies.map((allergy) => {
-            return(<li key={allergy.id}>{allergy.allergen} ({allergy.reaction})</li>)
-          })}
-        </ul>
+      <TitledCard title="Allergies" color='#9F3494'>
+        <div>
+        {ptInfo.allergies.map((allergy) => {
+          return (
+            <div key={allergy.id}>
+              <span style={{ color: '#9F3494'}}>{allergy.allergen}</span> {allergy.reaction}
+                  </div>
+                );
+              })}
+        </div>
       </TitledCard>
-      <TitledCard title="Medical History">
-        <ul>
+      <TitledCard title="Medical History" color='#9F3494'>
+        <div>
           {ptInfo.medicalHistory.map((condition) => {
-            return(<li key={condition.id}>{condition.name}</li>)
+            return (
+              <div key={condition.id} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <span style={{ color: condition.date === "Date Unknown" ? '#bbbbbb' : 'inherit', textAlign: 'right', minWidth: '110px' }}>
+                  {condition.date}
+                </span>
+                <span style={{ flex: '1', textAlign: 'left', marginLeft: '25px' }}>{condition.name}</span>
+              </div>
+            );
           })}
-        </ul>
+        </div>
       </TitledCard>
-      <TitledCard title="Medications">
-        <ul>
+      <TitledCard title="Medications" color='#9E49E2'>
+        <div>
           {ptInfo.medications.map((medication) => {
-            return(<li key={medication.id}>{medication.name} {medication.dosage} {medication.frequency}</li>)
+            return(<div key={medication.id} style={{ color: '#9E49E2' }}>{medication.name} {medication.dosage} {medication.frequency}</div>)
           })}
-        </ul>
+        </div>
       </TitledCard>
-      <TitledCard title="Surgical History">
-        <ul>
-          {ptInfo.surgicalHistory.map((procedure) => {
-            return(<li key={procedure.id}>{procedure.name} ({(new Date(procedure.date)).toLocaleDateString("en-US")})</li>)
+      <TitledCard title="Surgical History" color= '#9F3494'>
+        <div>
+          {ptInfo.surgicalHistory.map((condition) => {
+            return (
+              <div key={condition.id} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <span style={{ color: condition.date === "Date Unknown" ? '#bbbbbb' : 'inherit', textAlign: 'right', minWidth: '110px' }}>
+                  {condition.date}
+                </span>
+                <span style={{ flex: '1', textAlign: 'left', marginLeft: '25px' }}>{condition.name}</span>
+              </div>
+            );
           })}
-        </ul>
+        </div>
       </TitledCard>
-      <TitledCard title="Family History">
-        <ul>
+      <TitledCard title="Family History" color= '#9F3494'>
+        <div>
           {ptInfo.familyHistory.map((relative) => {
-            return(<li key={relative.id}>{relative.relation} ({relative.conditions.join(' ')})</li>)
+            return(<div key={relative.id}> <span style={{color:'#bbbbbb'}}>{relative.relation}</span> <span style={{marginLeft:'35px'}}>{relative.conditions}</span></div>)
           })}
-        </ul>
+        </div>
       </TitledCard>
     </div>
   );
