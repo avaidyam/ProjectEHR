2bae1aa959315222d582b0a174d8c1e01c23219f
Aditya Vaidyam <avaidyam@bidmc.harvard.edu>
2025-09-29 21:29:41 -0500

    cleanup

--- src/routes/Patient/routes/NoteViewer/NoteViewer.jsx
+++ src/routes/Patient/routes/NoteViewer/NoteViewer.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Box, Stack, Divider, Label, RichText } from 'components/ui/Core.jsx';
+import { Box, Stack, Divider, Label, RichText } from 'components/ui/Core';
 
 const NoteViewer = ({ selectedRow }) => {
-  const incomplete = selectedRow.data["status"] === "Incomplete"
+  const incomplete = selectedRow.data.status === "Incomplete"
   return (
     <Stack direction="column" sx={{ p: 2 }}>
       <Divider />
       <Stack direction="row" spacing={4}>
         <Stack direction="column">
-          <Label bold>{selectedRow.data["author"]}</Label>
-          <Label>{selectedRow.data["encDept"]}</Label>
+          <Label bold>{selectedRow.data.author}</Label>
+          <Label>{selectedRow.data.encDept}</Label>
           <Label>Physician</Label>
         </Stack>
         <Stack direction="column">
-          <Label>{selectedRow.data["noteType"]}</Label>
-          <Label>{selectedRow.data["status"]}</Label>
+          <Label>{selectedRow.data.noteType}</Label>
+          <Label>{selectedRow.data.status}</Label>
         </Stack>
       </Stack>
-      <Label><span style={{ color: '#888' }}>Date of Service:</span> {selectedRow.data["filingDate"]}</Label>
+      <Label><span style={{ color: '#888' }}>Date of Service:</span> {selectedRow.data.filingDate}</Label>
       <Divider sx={{ mb: 1 }} />
       <Label bold sx={incomplete ? { color: 'error.main' } : { display: "none" } }>Incomplete</Label>
       <Box sx={{ p: 1, pt: 0, ...(incomplete ? { borderLeft: 4, borderColor: "error.main" } : {}) }}>
