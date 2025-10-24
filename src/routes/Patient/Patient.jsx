f471f4c4f093676ce9b61a26b1276ccfa3f3ed49
lzhaxi <lzha.xlii@gmail.com>
2025-10-23 22:11:54 -0500

    Initial commit, data structure is department-level now

--- src/routes/Patient/Patient.jsx
+++ src/routes/Patient/Patient.jsx
@@ -23,6 +23,7 @@ import LabReport from './routes/LabReport/LabReport.jsx';
 import ImagingViewer from './routes/ImagingViewer/ImagingViewer.jsx';
 import NoteViewer from './routes/NoteViewer/NoteViewer.jsx';
 import ClinicalImpressions from './routes/ClinicalImpressions/ClinicalImpressions.jsx';
+import Handoff from './routes/Handoff/Handoff.jsx';
 import Demographics from './routes/Demographics/Demographics.jsx';
 import BottomBar from './components/BottomBar.jsx';
 
@@ -39,6 +40,7 @@ const ALL_TABS = {
   "PDMP": (props) => <Pdmp {...props} />,
   "Immunizations": (props) => <Immunizations {...props} />,
   "Clinical Impressions": (props) => <ClinicalImpressions {...props} />,
+  "Handoff": (props) => <Handoff {...props} />,
   "Allergies": (props) => <Allergies {...props} />,
   "Demographics": (props) => <Demographics {...props} />,
   "Chat": (props) => <Chat {...props} />,
@@ -51,7 +53,7 @@ const DEFAULT_MAIN_TABS = [
   { "SnapShot": {} }, { "Chart Review": {} }, { "Problem List": {} },
   { "History": {} }, { "Medications": {} }, { "Orders Mgmt": {} },
   { "NoteWriter": {} }, { "Results Review": {} }, { "Immunizations": {} },
-  { "Allergies": {} }, { "Demographics": {} }, { "Clinical Impressions": {} }
+  { "Handoff": {} }, { "Allergies": {} }, { "Demographics": {} }, { "Clinical Impressions": {} }
 ]
 
 const DEFAULT_SIDE_TABS = [
